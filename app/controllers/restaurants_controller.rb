bc99cc767b18c682b1fae836ea2c2eeb9f542bb0
Pablo Maisuls <pablo_maisuls@live.com>
2019-11-27 13:00:17 +0100

    first version of yelp mvp

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,42 @@
+class RestaurantsController < ApplicationController
+
+    def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(task_params)
+    @restaurant.save
+    redirect_to restaurants_path
+  end
+
+  def edit
+    @@restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @@restaurant = Restaurant.find(params[:id])
+    @@restaurant.update(@restaurant_params)
+    redirect_to restaurants_path
+  end
+
+   def destroy
+    @@restaurant = Restaurant.find(params[:id])
+    @@restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+private
+
+  def task_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+get 'restaurants', to: 'restaurants#index', as: :restaurants
+
+get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+post 'restaurants', to: 'restaurants#create'
+
+get 'restaurants/:id/reviews/new', to: 'reviews#new', as: :new_review
+post 'restaurants/:id/reviews', to: 'reviews#create'
+
+get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
 end
